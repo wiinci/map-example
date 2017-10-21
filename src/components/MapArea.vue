495cbe70ba1942e9581cbee4113afdc3deda8965
Mardav Wala <tru.vinci@gmail.com>
2017-10-21 14:16:15 -0400

    Fix marker

--- src/components/MapArea.vue
+++ src/components/MapArea.vue
@@ -10,7 +10,7 @@
                 :center="[place.longitude, place.latitude]">
                 <v-tilelayer :url="url"
                     :attribution="attribution"></v-tilelayer>
-                <v-marker :lat-lng="{ lng: place.longitude, lat: place.latitude }"></v-marker>
+                <v-marker :lat-lng="[place.longitude, place.latitude]"></v-marker>
             </v-map>
         </div>
     </div>
@@ -18,6 +18,19 @@
 
 <script>
 import Vue2Leaflet from 'vue2-leaflet';
+
+// Workaround for Map Marker
+// Inlined Base64 image appears broken
+// https://github.com/Leaflet/Leaflet/issues/4968
+// https://github.com/KoRiGaN/Vue2Leaflet/issues/28#issuecomment-320227460
+import L from 'leaflet';
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.imagePath = '';
+L.Icon.Default.mergeOptions({
+    iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+    iconUrl: require('leaflet/dist/images/marker-icon.png'),
+    shadowUrl: require('leaflet/dist/images/marker-shadow.png')
+});
 
 export default {
     name: 'map-area',
